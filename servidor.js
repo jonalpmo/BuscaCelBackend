6e39bce9d341ad76860226aa1f36686ab095ffea
Jonathan Alpizar <jonalpmo@gmail.com>
2018-07-25 19:13:30 -0600

    create added

--- servidor.js
+++ servidor.js
@@ -13,6 +13,25 @@ app.get('/', (req, res)=> {
   res.send('Server on')
 })
 
+// crear celulares
+app.post('/api/create',(req,res)=>{
+  const {marca,modelo,sistemaOperativo} = req.body
+  let nuevoCelular = Celular({
+    marca,
+    modelo,
+    sistemaOperativo
+  })
+
+  nuevoCelular.save((error,celular)=>{
+    res.status(201).send(celular)
+  });
+
+});
+
+
+
+
+
 app.listen(PORT, () => {
   console.log('Running on port ' + PORT);
 })
