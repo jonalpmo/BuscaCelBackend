947dfe4bd8804566d8ddb29f0952c0778744e06c
Geoffrey Bery <gbbery@gmail.com>
2018-07-25 20:19:09 -0500

    second commitj

--- servidor.js
+++ servidor.js
@@ -1,18 +1,26 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const bodyParser = require('body-parser');
+const Celular = require('./mongooseClient');
 const PORT = process.env.PORT || 3000
 
-
-app.use(bodyParser.urlencoded({extends:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.get('/api/celulares', (req,res)=>{
+        Alumno.find().exec()
+            .then(celular=> res.send(celular))
+            .catch(err => res.send(err))
+    });
 
+app.get('/api/celulares/:uid', (req,res)=>{
+    let {uid} = req.params;
+    Celular.findById(uid).exec()
+        .then(celular => res.send(celular))
+        .catch(err => res.send(err))
+});
 
-app.get('/', (req, res)=> {
-  res.send('Server on')
-})
+app.listen(3000, () => {
+    console.log('Running on port '+ PORT)
+});
 
-app.listen(PORT, () => {
-  console.log('Running on port ' + PORT);
-})
